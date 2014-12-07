6742f1eac2914bd32c09064a5dafe87775d857b8
overallduka <overallduka@hotmail.com>
2014-12-07 11:19:40 -0200

    Added scaffold of template email, to create, view and edit

--- app/controllers/email_templates_controller.rb
+++ app/controllers/email_templates_controller.rb
@@ -2,22 +2,41 @@ class EmailTemplatesController < ApplicationController
     respond_to :js, :html,:json
 
   def index
+    @email_templates = EmailTemplate.all
   end
 
   def create
+    @email_template = EmailTemplate.new email_template_params
+    @email_template.save
+    redirect_to email_templates_path
+  end
+
+  def show
+    @email_template = EmailTemplate.find(params[:id])    
   end
 
   def new
+    @email_template = EmailTemplate.new
   end
 
   def get
-        @template = EmailTemplate.find(params[:id])
-        respond_with @template
-    end
+    @template = EmailTemplate.find(params[:id])
+    respond_with @template
+  end
+
+  def edit
+    @email_template = EmailTemplate.find(params[:id])    
+  end
+
+  def update
+    @template = EmailTemplate.find(params[:id])
+    @template.update_attributes(email_template_params)
+    redirect_to email_templates_path
+  end
 
     
-    def email_template_params
-      params.require(:email_template).permit(:content, :name)
-    end
+  def email_template_params
+    params.require(:email_template).permit(:content, :name)
+  end
 
 end
